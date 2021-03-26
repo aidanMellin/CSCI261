6c55ccb0103478e6d37ba1ba361dda2be3210a9c
aMellin <aidan.mellin@gmail.com>
2021-03-26 12:31:44 -0400

    wow im struggling

--- HW4/Kruskal.java
+++ HW4/Kruskal.java
@@ -3,85 +3,136 @@ package git.CSCI261.HW4;
 import java.io.*;
 import java.util.*;
 
-class Edge implements Comparable<Edge> {
-    private int v1, v2, weight;
-
-    public Edge(int vertex1, int vertex2, int cost) {
-        this.v1 = vertex1;
-        this.v2 = vertex2;
-        this.weight = cost;
-    }
-
-    public int getV1() {
-        return this.v1;
-    }
-
-    public int getV2() {
-        return this.v2;
-    }
-
-    public int getWeight() {
-        return this.weight;
-    }
-
-    @Override
-    public int compareTo(Edge other) {
-        return this.getWeight() - other.getWeight();
-    }
-}
+import jdk.incubator.foreign.ValueLayout;
 
 public class Kruskal {
-    static ArrayList<Edge> edgeList; // TODO
-    static int nodeCount; // TODO
+    static Edge edgeList[];
+    static int vCount, eCount;
 
-    public void minSpanTree() {
-        Collections.sort(edgeList);
-        ArrayList<Edge> mstEdges = new ArrayList<Edge>();
-        DisjSets nodeSet = new DisjSets(nodeCount);
+    class Sub {
+        int root, rank;
 
-        for (int i = 0; i < edgeList.size() && mstEdges.size() < (nodeCount); i++) {
-            Edge curr = edgeList.get(i);
-            int r1 = nodeSet.find(curr.getV1());
-            int r2 = nodeSet.find(curr.getV2());
-            if (r1 != r2) {
-                mstEdges.add(curr);
-                nodeSet.union(r1, r2);
-            }
+        public Sub() {
+        }
+
+        public Sub(int i) {
+            this.root = i;
+            this.rank = 0;
         }
     }
 
-    static ArrayList<Edge> file_process(String fn) {
-        ArrayList<Edge> rtn = new ArrayList<Edge>();
+    class Edge implements Comparable<Edge> {
+        public int src, dst, weight;
+
+        @Override
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    }
+
+    Kruskal(int e) {
+        eCount = e;
+        edgeList = new Edge[eCount];
+        for (int i = 0; i < eCount; i++) {
+            edgeList[i] = new Edge();
+        }
+    }
+
+    static void file_process(String fn) {
         try {
             File f = new File(fn);
             BufferedReader fp = new BufferedReader(new FileReader(f));
             String line;
-            nodeCount = Integer.parseInt(fp.readLine());
-            System.out.println(nodeCount);
+            vCount = Integer.parseInt(fp.readLine());
+            int i = 0;
+            edgeList = new Edge[vCount * (vCount - 1)];
+            for (int j = 0; j < edgeList.length; j++)
+                edgeList[j] = new Edge();
+
             while ((line = fp.readLine()) != null) {
                 String splitLine[] = line.split(" ");
-                int src = Integer.parseInt(splitLine[0].replaceAll("Node\\[(.*?)\\]:", "$1")) - 1;
+                int source = Integer.parseInt(splitLine[0].replaceAll("Node\\[(.*?)\\]:", "$1")) - 1;
                 List<String> content = new ArrayList<String>(Arrays.asList(splitLine));
                 content.remove(0);
-                for (String s : content) {
-                    String tmp[] = s.split(":");
-                    rtn.add(new Edge(Integer.parseInt(tmp[1]), src, Integer.parseInt(tmp[0]) - 1));
+                for (int s = 0; s < content.size(); s++) {
+                    String tmp[] = content.get(s).split(":");
+                    edgeList[i].src = source;
+                    edgeList[i].dst = Integer.parseInt(tmp[0]) - 1;
+                    edgeList[i].weight = Integer.parseInt(tmp[1]);
+                    i++;
                 }
             }
             fp.close();
-            return rtn;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return rtn;
+    }
+
+    int findC(Sub set[], int index) { // Find with path compression
+        if (set[index].root != index)
+            set[index].root = findC(set, set[index].root);
+        return set[index].root;
+    }
+
+    void union(Sub set[], int r1, int r2) { // Union by rank
+        int Root1 = findC(set, r1);
+        int Root2 = findC(set, r2);
+        if (set[Root1].rank < set[Root2].rank)
+            set[Root1].root = Root2;
+        else {
+            if (set[Root1] == set[Root2])
+                set[Root1].rank++;
+            set[Root2].root = Root1;
+        }
+    }
+
+    void quicksort() {
+        for (int i = 0; i < edgeList.length - 1; i++) {
+            for (int j = 0; j < edgeList.length - 1; j++) {
+                if (edgeList[j].weight > edgeList[j + 1].weight) {
+                    Edge temp = edgeList[j];
+                    edgeList[j] = edgeList[j + 1];
+                    edgeList[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void constructMST() {
+        Edge res[] = new Edge[vCount];
+        int index = 0;
+        int i = 0;
+        for (i = 0; i < vCount; i++)
+            res[i] = new Edge();
+        Arrays.sort(edgeList);
+
+        Sub set[] = new Sub[vCount];
+        for (i = 0; i < vCount; i++)
+            set[i] = new Sub(i);
+
+        i = 0;
+        while (index < vCount - 1) {
+            Edge next = new Edge();
+            next = edgeList[i++];
+
+            int r1 = findC(set, next.src);
+            int r2 = findC(set, next.dst);
+
+            if (r1 != r2) {
+                res[index++] = next;
+                union(set, r1, r2);
+            }
+
+        }
     }
 
     public static void main(String[] args) {
-        Kruskal krusk = new Kruskal();
         String fn = args[0];
-        ArrayList<Edge> edges = file_process(fn);
-        krusk.edgeList = edges;
-        krusk.minSpanTree();
+
+        int e = edgeList.length;
+        Kruskal krusk = new Kruskal(e);
+        file_process(fn);
+        krusk.constructMST();
     }
 
 }
